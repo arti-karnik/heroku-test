2c74c682f654144d8a1e4dd9ecbffb4639841c52
Arti <arti.iosdev@gmail.com>
2021-03-10 19:18:55 -0800

    added

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
 
 app.get("/", function(req, res) {
-    return false.readFile(path.join(__dirname, "index.html"), (e, data
+    return fs.readFile(`${__dirname}/index.html`, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      });
+
+    /*return false.readFile(path.join(__dirname, "index.html"), (e, data
         ) => {
             if (e) console.error(e);
             res.writeHead(200, {'Content-Type': "text/html"});
             res.end(data);
 
-        })
+        })*/
+        
    
 
     //res.json(path.join(__dirname, "public/index.html"));
